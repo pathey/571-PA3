e84ebf93c15762d8a4a2ed365d3f92a5678f31b6
pathey <peter@athey.org>
2025-11-20 11:37:02 -0800

    Added auto ingestion of system config

--- PA3.py
+++ PA3.py
@@ -29,18 +29,41 @@ def EE_EDF_scheduler():
 def EE_RM_scheduler():
 	pass
 
-def system_constructor(system_line):
-	pass
+def system_constructor(system_line) -> SystemConfig:
+	parts = system_line.split()
+	
+	task_quantity = int(parts[0])
+	time_limit = int(parts[1])
+	active_power = {
+		1188:int(parts[2]), 
+		918:int(parts[3]), 
+		648:int(parts[4]), 
+		384:int(parts[5])
+	}
+	idle_power = int(parts[6])
+
+	return SystemConfig(
+		task_quantity = task_quantity,
+		time_limit=time_limit,
+		active_power = active_power,
+		idle_power = idle_power
+	)
 
 def task_constructor(task_line):
 	pass
 
 input_file = sys.argv[1]
 policy = sys.argv[2]
+energy_efficient = None
+
+if len(sys.argv) > 3:
+	energy_efficient = sys.argv[3]
 
 with open(input_file, "r") as f:
 	system_line = f.readline()
-	system_constructor(system_line)
+	sys_conf = system_constructor(system_line)
 
 	for line in f:
 		task_constructor(line)
+
+print(sys_conf)
